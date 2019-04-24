13249cc3dd313f3dc4a10f7ffa71ecb6b58b82e9
joaogui1 <joaoguilhermearujo@gmail.com>
2019-04-24 08:05:16 -0300

    repr

--- measurement.py
+++ measurement.py
@@ -22,15 +22,17 @@ class Measurement:
         return ret
     def __str__(self):
         return f'{self.val} ± {self.delta}'
+    def __repr__(self):
+        return f'{self.val} ± {self.delta}'
 
-d = Measurement(9.7*(10**-4), 0.00005)
-b = Measurement(0.02475, 0.00005)
-d3 = d**3
-w = Measurement(3.716, 9.81*10**(-5))
-p = d3*b*(2.86)
-E = w*4
-E /= p
-E *= 1/(10**10)
-
-
-print(E)
+# d = Measurement(9.7*(10**-4), 0.00005)
+# b = Measurement(0.02475, 0.00005)
+# d3 = d**3
+# w = Measurement(3.716, 9.81*10**(-5))
+# p = d3*b*(2.86)
+# E = w*4
+# E /= p
+# E *= 1/(10**10)
+#
+#
+# print(E)
