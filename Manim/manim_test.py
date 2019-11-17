9a24e0c3d01c36eabc9656b8aafb30b0e05661b6
joaogui1 <joaoguilhermearujo@gmail.com>
2019-11-17 22:22:08 +0900

    more shapes

--- Manim/manim_test.py
+++ Manim/manim_test.py
@@ -8,8 +8,8 @@ class Shapes(Scene):
         line = Line(np.array([3,0,0]),np.array([5,0,0]))
         triangle = Polygon(np.array([0,0,0]),np.array([1,1,0]),np.array([1,-1,0]))
 
-        self.add(line)
         self.play(ShowCreation(circle))
         self.play(Transform(circle, square))
         # self.play(FadeOut(circle))
+        self.add(line)
         self.play(Transform(circle, triangle))
