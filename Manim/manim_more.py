92b3460a7a278a05fd922cc08aedabd92ca56840
joaogui1 <joaoguilhermearujo@gmail.com>
2019-11-18 22:43:16 +0900

    more shapes

--- Manim/manim_more.py
+++ Manim/manim_more.py
@@ -1,23 +1,23 @@
+import numpy as np
 from manimlib.imports import *
 
 class MoreShapes(Scene):
     def construct(self):
         circle = Circle(color=PURPLE_A)
-        square = Square(fill_color=GOLD_B, fill_opacity=1, color=GOLD_A)
-        square.move_to(UP+LEFT)
-        circle.surround(square)
         rectangle = Rectangle(height=2, width=3)
-        ellipse=Ellipse(width=3, height=1, color=RED)
+        ellipse = Ellipse(width=3, height=1, color=RED)
         ellipse.shift(2*DOWN+2*RIGHT)
-        pointer = CurvedArrow(2*RIGHT,4*RIGHT,color=MAROON_C)
+        pointer = CurvedArrow(2*UP,4*RIGHT,color=MAROON_C)
         arrow = Arrow(LEFT,UP)
         arrow.next_to(circle,DOWN+LEFT)
         rectangle.next_to(arrow,DOWN+LEFT)
-        ring=Annulus(inner_radius=.5, outer_radius=1, color=BLUE)
-        ring.next_to(ellipse, RIGHT)
-
+        ring = Annulus(inner_radius=np.sqrt(2), outer_radius=2, color=BLUE)
+        # ring.next_to(ellipse, RIGHT)
+        pentagon = Polygon(np.array([-0.8 ,-0.8 ,0]), np.array([-1.4 ,0.85, 0]), np.array([0,np.sqrt(2) + 0.4,0]), np.array([1.4, 0.85, 0]), np.array([0.8, -0.8, 0]))
+        ring.surround(pentagon)
+        
         self.add(pointer)
-        self.play(FadeIn(square))
-        self.play(Rotating(square),FadeIn(circle))
+        self.play(FadeIn(pentagon))
         self.play(GrowArrow(arrow))
         self.play(GrowFromCenter(rectangle), GrowFromCenter(ellipse), GrowFromCenter(ring))
+        self.play(Rotating(pentagon))
