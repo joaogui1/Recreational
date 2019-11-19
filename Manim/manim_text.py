b4436ff29135ad6958650a02e6e07fc622914c55
joaogui1 <joaoguilhermearujo@gmail.com>
2019-11-19 22:50:43 +0900

    Text

---
+++ Manim/manim_text.py
@@ -0,0 +1,18 @@
+import numpy as np
+from manimlib.imports import *
+
+class AddingText(Scene):
+    #Adding text on the screen
+    def construct(self):
+        my_first_text=TextMobject("Writing with manim is fun")
+        second_line=TextMobject("and easy to do!")
+        second_line.next_to(my_first_text,DOWN)
+        third_line=TextMobject("for me and you!")
+        third_line.next_to(my_first_text,DOWN)
+
+        self.add(my_first_text, second_line)
+        self.wait(2)
+        self.play(Transform(second_line,third_line))
+        self.wait(2)
+        self.play(ApplyMethod(second_line.shift,3*DOWN))
+        self.play(ApplyMethod(my_first_text.shift,3*UP))
