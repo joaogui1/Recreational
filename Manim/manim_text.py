8fa546dfb69475a3dbb4840f3b2c879593102cd7
joaogui1 <joaoguilhermearujo@gmail.com>
2019-11-21 10:04:03 +0900

    More text methods

--- Manim/manim_text.py
+++ Manim/manim_text.py
@@ -6,9 +6,9 @@ class AddingText(Scene):
     def construct(self):
         my_first_text=TextMobject("Writing with manim is fun")
         second_line=TextMobject("and easy to do!")
-        second_line.next_to(my_first_text,DOWN)
+        second_line.next_to(my_first_text, DOWN)
         third_line=TextMobject("for me and you!")
-        third_line.next_to(my_first_text,DOWN)
+        third_line.next_to(my_first_text, DOWN)
 
         self.add(my_first_text, second_line)
         self.wait(2)
@@ -16,3 +16,47 @@ class AddingText(Scene):
         self.wait(2)
         self.play(ApplyMethod(second_line.shift,3*DOWN))
         self.play(ApplyMethod(my_first_text.shift,3*UP))
+
+class AddingMoreText(Scene):
+    #Playing around with text properties
+    def construct(self):
+        quote = TextMobject("Imagination is more important than knowledge")
+        quote.set_color(RED)
+        quote.to_edge(UP)
+        quote2 = TextMobject("A person who never made a mistake never tried anything new")
+        quote2.set_color(YELLOW)
+        author=TextMobject("-Albert Einstein")
+        author.scale(0.75)
+        author.next_to(quote.get_corner(DOWN+RIGHT),DOWN)
+
+        self.add(quote)
+        self.add(author)
+        self.wait(2)
+        self.play(Transform(quote, quote2),
+          ApplyMethod(author.move_to,quote2.get_corner(DOWN+RIGHT)+DOWN+2*LEFT))
+        
+        self.play(ApplyMethod(author.scale, 1.5))
+        author.match_color(quote2)
+        self.play(FadeOut(quote))
+
+class RotateAndHighlight(Scene):
+    #Rotation of text and highlighting with surrounding geometries
+    def construct(self):
+        square=Square(side_length=5,fill_color=YELLOW, fill_opacity=1)
+        label=TextMobject("Text at an angle")
+        label.bg=BackgroundRectangle(label,fill_opacity=1)
+        label_group=VGroup(label.bg,label)  #Order matters
+        label_group.rotate(TAU/8)
+        label2=TextMobject("Boxed text",color=BLACK)
+        label2.bg=SurroundingRectangle(label2,color=BLUE,fill_color=RED, fill_opacity=.5)
+        label2_group=VGroup(label2,label2.bg)
+        label2_group.next_to(label_group,DOWN)
+        label3=TextMobject("Rainbow")
+        label3.scale(2)
+        label3.set_color_by_gradient(RED, ORANGE, YELLOW, GREEN, BLUE, PURPLE)
+        label3.to_edge(DOWN)
+
+        self.add(square)
+        self.play(FadeIn(label_group))
+        self.play(FadeIn(label2_group))
+        self.play(FadeIn(label3))
