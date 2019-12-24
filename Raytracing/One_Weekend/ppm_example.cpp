2de445f02d7788aba92aece1fa13a8565e52d759
joaogui1 <joaoguilhermearujo@gmail.com>
2019-12-24 22:19:46 +0900

    vec3

--- Raytracing/One_Weekend/ppm_example.cpp
+++ Raytracing/One_Weekend/ppm_example.cpp
@@ -1,18 +1,17 @@
 #include<iostream>
+#include "vec3.h"
 
 using namespace std;
 
 int main(){
-  int nx = 200, ny = 100;
+  int nx = 400, ny = 200;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
   for (int j = ny - 1; j >= 0; j--){
     for (int i = 0; i < nx; i++){
-      float r = float(i) / float(nx);
-      float g = float(j) / float(ny);
-      float b = 0.2;
-      int ir = int(255.99 * r);
-      int ig = int(255.99 * g);
-      int ib = int(255.99 * b);
+      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+      int ir = int(255.99 * col[0]);
+      int ig = int(255.99 * col[1]);
+      int ib = int(255.99 * col[2]);
       std::cout << ir << " " << ig << " " << ib << "\n";
     }
   }
