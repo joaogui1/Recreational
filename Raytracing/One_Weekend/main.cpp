8a8ac29d1206c63e6f9c10a567521184f3eeb559
joaogui1 <joaoguilhermearujo@gmail.com>
2019-12-26 00:05:07 +0900

    msg

--- Raytracing/One_Weekend/main.cpp
+++ Raytracing/One_Weekend/main.cpp
@@ -1,13 +1,24 @@
 #include<iostream>
 #include "sphere.h"
+#include "camera.h"
+#include "random.h"
 #include "hittable_list.h"
 
 using namespace std;
 
+vec3 random_in_unit_sphere(){
+  vec3 p;
+  do{
+    p = 2.0 * vec3(random_double(), random_double(), random_double()) - vec3(1, 1, 1);
+  } while (p.squared_length() >= 1.0);
+  return p;
+}
+
 vec3 color(const ray &r, hittable *world){
   hit_record rec;
-  if (world->hit(r, 0.0, MAXFLOAT, rec)){
-    return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+  if (world->hit(r, 0.001, MAXFLOAT, rec)){
+    vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+    return 0.5 * color(ray(rec.p, target - rec.p), world);
   }
   else{
     vec3 unit_direction = unit_vector(r.direction());
@@ -17,7 +28,7 @@ vec3 color(const ray &r, hittable *world){
 }
 
 int main(){
-  int nx = 200, ny = 100;
+  int nx = 2200, ny = 1100, ns = 10;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
   vec3 lower_left_corner(-2.0, -1.0, -1.0);
   vec3 horizontal(4.0, 0.0, 0.0);
@@ -28,15 +39,18 @@ int main(){
   list[0] = new sphere(vec3(0, 0, -1), 0.5);
   list[1] = new sphere(vec3(0, -100.5, -1), 100);
   hittable *world = new hittable_list(list, 2);
+  camera cam;
   for (int j = ny - 1; j >= 0; j--){
     for (int i = 0; i < nx; i++){
-      float u = float(i) / float(nx);
-      float v = float(j) / float(ny);
-      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
-
-      vec3 p = r.point_at_parameter(2.0);
-      vec3 col = color(r, world);
-
+      vec3 col(0, 0, 0);
+      for (int s = 0; s < ns; s++){
+        float u = float(i + random_double()) / float(nx);
+        float v = float(j + random_double()) / float(ny);
+        ray r = cam.get_ray(u, v);
+        col += color(r, world);
+      }
+      col /= float(ns);
+      col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
