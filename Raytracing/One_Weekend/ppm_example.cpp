1670d870bb6415e171a1f81866e55bf8e33c6f1c
joaogui1 <joaoguilhermearujo@gmail.com>
2019-12-25 22:46:15 +0900

    color

--- Raytracing/One_Weekend/ppm_example.cpp
+++ Raytracing/One_Weekend/ppm_example.cpp
@@ -1,14 +1,29 @@
 #include<iostream>
-#include "vec3.h"
+#include "ray.h"
 
 using namespace std;
+
+vec3 color(ray r){
+  float y = unit_vector(r.direction()).y();
+  float t = (y + 1.0)/2.0;
+  vec3 blue = vec3(0.0, 0.0, 1.0);
+  vec3 white = vec3(1.0, 1.0, 1.0);
+  return (1 - t)*white + t*blue;  
+}
 
 int main(){
   int nx = 200, ny = 100;
   cout << "P3\n" << nx << " " << ny << "\n255\n";
+  vec3 lower_left_corner(-2.0, -1.0, -1.0);
+  vec3 horizontal(4.0, 0.0, 0.0);
+  vec3 vertical(0.0, 2.0, 0.0);
+  vec3 origin(0.0, 0.0, 0.0);
   for (int j = ny - 1; j >= 0; j--){
     for (int i = 0; i < nx; i++){
-      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
+      float u = float(i) / float(nx);
+      float v = float(j) / float(ny);
+      ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+      vec3 col = color(r);
       int ir = int(255.99 * col[0]);
       int ig = int(255.99 * col[1]);
       int ib = int(255.99 * col[2]);
