9e8f0116bb133dfc518f3e89d553d11fdc09c452
joaogui1 <joaoguilhermearujo@gmail.com>
2020-09-07 15:24:02 -0300

    lc1

---
+++ lc2.jl
@@ -0,0 +1,27 @@
+### A Pluto.jl notebook ###
+# v0.11.13
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 633717a8-f217-11ea-157c-33718fb150c1
+element = 1.0
+
+# ╔═╡ 7277ca5c-f217-11ea-12c0-15f57efbb1fc
+fill(element, 3, 4)
+
+# ╔═╡ 779e146c-f217-11ea-2ef9-5f54f69d9e50
+typeof(element)
+
+# ╔═╡ 7cbe1a7a-f217-11ea-2891-d90852c145c0
+keeptrack = [typeof(1), typeof(1.0), typeof("one"), typeof(1//1)]
+
+# ╔═╡ 8a6af4f2-f217-11ea-1bf3-c920754a7151
+
+
+# ╔═╡ Cell order:
+# ╠═633717a8-f217-11ea-157c-33718fb150c1
+# ╠═7277ca5c-f217-11ea-12c0-15f57efbb1fc
+# ╠═779e146c-f217-11ea-2ef9-5f54f69d9e50
+# ╠═7cbe1a7a-f217-11ea-2891-d90852c145c0
+# ╠═8a6af4f2-f217-11ea-1bf3-c920754a7151
